2ad0b64ffcd53941a817a488715b3b408934daec
Alexandru-Cristian Mustata <106619900+MustataAlexandru@users.noreply.github.com>
2024-11-01 20:17:33 +0200

    build

--- app/navbar/UserIcon.tsx
+++ app/navbar/UserIcon.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { LuUser2 } from 'react-icons/lu';
 import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
 
 async function UserIcon() {
   // const {userId} = auth();
@@ -8,7 +9,7 @@ async function UserIcon() {
   const profileImage = user?.imageUrl;
   
   if(profileImage){
-    return <img src={profileImage} alt='user profile' className='w-6 h-6 rounded-full object-cover' />
+    return <Image src={profileImage} alt='user profile' className='w-6 h-6 rounded-full object-cover' />
   }
   return (
     <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white'/>
